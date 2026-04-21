3fd88f970b1719860edf5ef851a28e504e297bd1
kimheajeong <kimheajeong0510@gmail.com>
2026-04-21 09:55:46 +0900

    feat:view 임시 저장

--- src/view/UserView.java
+++ src/view/UserView.java
@@ -11,7 +11,19 @@ public class UserView {
 
     public void insert() {
         System.out.println("== 전화번호 등록 ==");
-        
+        // 이름 : 무조건 한글만. 중간 공백 없이
+        System.out.println("이름 : ");
+        String name = scanner.next();
+        // 나이 : 0세~120세 사이값
+        System.out.println("나이 : ");
+        int age = scanner.nextInt();
+        // 주소
+        System.out.println("주소 : ");
+        String address = scanner.next();
+        // 전화번호(010-xxxx-xxxx)
+        System.out.println("전화번호 : ");
+        String phone = scanner.next();
+
     }
 
     public void update() {
