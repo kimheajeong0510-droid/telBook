3cab09528947d8df6df75481b2595ef90ad0d483
kimheajeong <kimheajeong0510@gmail.com>
2026-04-21 09:45:49 +0900

    userview 생성

---
+++ src/view/UserView.java
@@ -0,0 +1,28 @@
+package view;
+
+import java.util.Scanner;
+
+public class UserView {
+    private final Scanner scanner;
+    public UserView(Scanner scanner){
+        this.scanner=scanner;
+        
+    }
+
+    public void insert() {
+        System.out.println("== 전화번호 등록 ==");
+        
+    }
+
+    public void update() {
+    }
+
+    public void delete() {
+    }
+
+    public void searchAll() {
+    }
+
+    public void searchOne() {
+    }
+}
